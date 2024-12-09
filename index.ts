28db1fa97c80ea35c4cd28dcc60e62a030bc8af3
hliebovkyrylo <kirilglebov06@gmail.com>
2024-12-09 17:47:44 +0200

    maded second task

--- index.ts
+++ index.ts
@@ -1,20 +1,52 @@
-function hasZeroSumSubarray(arr: number[]): boolean {
-  const prefixSumSet = new Set<number>();
-  let prefixSum = 0;
+function swapRowsWithMaxAndMinElements(matrix: number[][]): number[][] {
+  // Знаходимо індекс рядка з максимальним елементом
+  let maxRowIndex = 0;
+  let maxElement = matrix[0][0];
 
-  for (const num of arr) {
-    prefixSum += num;
+  // Знаходимо індекс рядка з мінімальним елементом
+  let minRowIndex = 0;
+  let minElement = matrix[0][0];
 
-    if (prefixSum === 0 || prefixSumSet.has(prefixSum)) {
-      return true;
+  // Пошук максимального та мінімального елементів
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] > maxElement) {
+        maxElement = matrix[i][j];
+        maxRowIndex = i;
+      }
+
+      if (matrix[i][j] < minElement) {
+        minElement = matrix[i][j];
+        minRowIndex = i;
+      }
     }
-
-    prefixSumSet.add(prefixSum);
   }
 
-  return false;
+  // Створюємо копію матриці для уникнення змін в оригіналі
+  const newMatrix = matrix.map((row) => [...row]);
+
+  // Міняємо рядки місцями
+  if (maxRowIndex !== minRowIndex) {
+    [newMatrix[maxRowIndex], newMatrix[minRowIndex]] = [
+      newMatrix[minRowIndex],
+      newMatrix[maxRowIndex],
+    ];
+  }
+
+  return newMatrix;
 }
 
-const array1 = [3, 4, -7, 1, 2, -1, 6];
-// const array2 = [1, 2, 3, 4, 5];
-console.log(hasZeroSumSubarray(array1));
+// Приклад використання
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+console.log("Матриця до перестановки:");
+console.table(matrix);
+
+const result = swapRowsWithMaxAndMinElements(matrix);
+
+console.log("Матриця після перестановки:");
+console.table(result);
